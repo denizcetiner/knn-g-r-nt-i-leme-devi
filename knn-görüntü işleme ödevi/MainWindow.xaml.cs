2242881a74b00da7f3b268ad669b02b2d98a6696
Deniz <Deniz@DESKTOP-IF0G3V9>
2016-12-18 19:26:05 +0300

    Add project files.

---
+++ knn-görüntü işleme ödevi/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace knn_görüntü_işleme_ödevi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<String> Dataset = new List<string>();
+        private String File = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        //örnek veriseti
+        public List<String> GetDir()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                return Directory.GetFiles(fbd.SelectedPath).AsEnumerable().ToList();
+            }
+            return GetDir();
+        }
+
+        //hesaplanacak veri
+        public String GetFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            return GetFile();
+        }
+
+        public Bitmap GetBitmap(String filename)
+        {
+            return new Bitmap(filename);
+        }
+
+        public double CompareBitmaps(Bitmap bmp1, Bitmap bmp2)
+        {
+            double sum = 0;
+            //x=width
+            for (int height = 0; height < bmp1.Height; height++)
+            {
+                for (int width = 0; width < bmp1.Width; width++)
+                {
+                    sum += CalculateDistance(bmp1.GetPixel(width, height), bmp2.GetPixel(width, height));
+                }
+            }
+            return sum;
+        }
+
+        public double CalculateDistance(System.Drawing.Color p1, System.Drawing.Color p2)
+        {
+            double distance = 0;
+
+            distance += Math.Abs(p1.B - p2.B);
+
+            return distance;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectDataset_Click(object sender, RoutedEventArgs e)
+        {
+            Dataset = GetDir();
+        }
+
+        private void SelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            File = GetFile();
+        }
+
+        private void ProcessFile_Click(object sender, RoutedEventArgs e)
+        {
+            Dictionary<String, double> Comparement = new Dictionary<string, double>();
+
+            foreach (String DatasetFile in Dataset)
+            {
+                Comparement.Add(
+                    DatasetFile,
+                    CompareBitmaps( GetBitmap(DatasetFile), GetBitmap(File) ) 
+                    );
+            }
+            var c = Comparement.OrderBy(element => element.Value);
+            Console.WriteLine("En çok şuna benziyor: " + c.ElementAt(0).Key);
+        }
+
+    }
+}
